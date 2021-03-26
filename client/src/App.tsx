358b16479d06a32242c15a17e1c1eaa9e99417e4
patrickliuhhs <patrickliu.hhs@gmail.com>
2021-03-25 22:48:51 -0400

    more progress on apollo/gql query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import {gql, useQuery} from '@apollo/client';
 
 function App() {
+  const searchInputRef = useRef<HTMLFormElement>(null);
+  // const searchCityRef = useRef<HTMLInputElement>(null);
+  // const searchZipRef = useRef<HTMLInputElement>(null);
 
+  interface QueryObj {
+    address: string;
+    city: string;
+    zipcode: string;
+  }
+
+  // console.log(searchAddressRef.current)
   const getResults = gql`
-    query GetResults {
-      text {
-        text
+    query GetResults ($city: String, $street: String, $zipcode: String ) {
+      landlords (city: $city, street: $street, zipcode: $zipcode){
+        name
+        id
+        city
+        state
+        street
+        zipcode
       }
     }
   `
-
-  const SearchRequest = (input: String) => {
+  const [query, setQuery] = useState<QueryObj| undefined> (undefined)
+  const { loading: boolean, data: object } = useQuery<any, QueryObj>(
+    getResults,
+    {variables: query}
+  );
   
-    const {loading, error, data} = useQuery(getResults);    
-    console.log('loading',loading);
-    console.log('error', error);
-    console.log('data', data);
+  const HandleClick = (event: Object) => {
+    console.log('poop');
+    let queryObj: QueryObj = {address: "default", city: "default", zipcode:"default"};
+    // let queryObj: QueryObj = {address: "default"};
+    // if (searchAddressRef && searchAddressRef.current && searchCityRef && searchCityRef.current && searchZipRef && searchZipRef.current) {
+    if (searchInputRef.current) {
+      console.log('searchAddRef.current',searchInputRef.current);
+      queryObj = {
+        address: searchInputRef.current['addressSearchBar'].value,
+        city: searchInputRef.current['citySearchBar'].value,
+        zipcode: searchInputRef.current['zipSearchBar'].value
+      };
+      console.log('queryObj', queryObj);
+      setQuery(queryObj);
+    }
 
+    // if (searchAddressRef.current && searchCityRef.current && searchZipRef.current) {
+    //   console.log('searchAddRef.current',searchAddressRef.current);
+    //   queryObj = {
+    //     address: searchAddressRef.current.value,
+    //     city: searchCityRef.current.value,
+    //     zipcode: searchZipRef.current.value
+    //   }
+    // }
+    console.log(queryObj);
+    
   }
-
-  const handleKeyPress = (event: String) => {
-
-  }
-
+ 
+  // return loading ? 
+  // (<Spinner />)
+  // :  
   return (
     <div className="App">
       <h1>RATEMYLANDLORDS</h1>
       <h2>Enter your city, zip code, or address to get started</h2>
-      <form noValidate autoComplete="off">
+      <form ref={searchInputRef} noValidate autoComplete="off">
         {/* <TextField id="standard-basic" label="Standard" /> */}
         {/* <TextField id="filled-basic" label="Filled" variant="filled" /> */}
-        <TextField id="addresssearchbar" label="Address" variant="outlined" />
-        <TextField id="citysearchbar" label="City" variant="outlined" />
-        <TextField id="zipsearchbar" label="Zip Code" variant="outlined" />
-        
+        <TextField name={"addressSearchBar"} label="Address" variant="outlined"/>
+        <TextField name={"citySearchBar"} label="City" variant="outlined"/>
+        <TextField name={"zipSearchBar"} label="Zip Code" variant="outlined"/>
       </form>
-      <Button variant="contained">Search</Button>
+      <Button variant="contained" onClick={()=>HandleClick({123:456})}>Search</Button>
     </div>
   );
 }
